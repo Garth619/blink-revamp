55a89aff4af357d0720f643cccd1dbc4def25e16
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-17 10:44:14 -0800

    push

---
+++ wp-content/themes/blink/homepage_template_parts/section-3.php
@@ -0,0 +1,24 @@
+<section id="section_three">
+	
+	
+	<div class="sec_three_box">
+		
+		
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box">
+		
+		
+		
+	</div><!-- sec_three_box -->
+	
+	<div class="sec_three_box">
+		
+		
+		
+	</div><!-- sec_three_box -->
+	
+	
+	
+</section><!-- section_three -->
