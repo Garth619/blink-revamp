2b52883099380551b20136b3de48f195fab7205a
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-17 09:56:04 -0800

    push

--- wp-content/themes/blink/homepage_template_parts/section-2.php
+++ wp-content/themes/blink/homepage_template_parts/section-2.php
@@ -1,6 +1,52 @@
 <section id="section_two">
 	
-	
+	<div class="sec_two_inner">
+		
+		<div class="sec_two_content">
+			
+			<?php echo file_get_contents("wp-content/themes/blink/images/blink-circle.svg"); ?>
+			
+			<span class="sec_two_intro">Dare to Be Fabulous Flirty and fierce with flawless lashes</span><!-- sec_two_intro -->
+			
+			<span class="sec_two_intro_content">For a look that says everything – your passions, aspirations, the truth of your heart – you deserve the best.  Blink Lash Boutique, a premier eyelash extensions salon founded in San Diego by two industry pioneers, is here to help you achieve your beauty without boundaries.  It’s time to let yourself be the version of yourself you imagine.</span><!-- sec_two_intro_content -->
+			
+		</div><!-- sec_two_content -->
+		
+		<div class="sec_two_gallery_wrapper">
+			
+			<div class="sec_two_gallery">
+				
+				<div class="single_gallery">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/gallery1.jpg"/>
+					
+				</div><!-- single_gallery -->
+				
+				<div class="single_gallery">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/gallery2.jpg"/>
+					
+				</div><!-- single_gallery -->
+				
+				<div class="single_gallery">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/gallery3.jpg"/>
+					
+				</div><!-- single_gallery -->
+				
+				<div class="single_gallery">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/gallery4.jpg"/>
+					
+				</div><!-- single_gallery -->
+				
+			</div><!-- sec_two_gallery -->
+			
+			
+		</div><!-- sec_two_gallery_wrapper -->
+		
+		
+	</div><!-- sec_two_inner -->
 	
 	
 </section><!-- section_two -->
