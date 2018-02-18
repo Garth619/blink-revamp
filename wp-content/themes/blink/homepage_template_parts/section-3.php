6fc06ab7ec5f566f6d074be1b6d65eae3c5c8720
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-17 16:54:22 -0800

    push

--- wp-content/themes/blink/homepage_template_parts/section-3.php
+++ wp-content/themes/blink/homepage_template_parts/section-3.php
@@ -3,19 +3,49 @@
 	
 	<div class="sec_three_box">
 		
+		<a href="">
 		
+		<div class="overlay">
+			
+			<span class="sec_box_title">Services</span><!-- sec_box_title -->
+			
+		</div><!-- overlay -->
+		
+		<img src="<?php bloginfo('template_directory');?>/images/services.jpg"/>
+		
+		</a>
 		
 	</div><!-- sec_three_box -->
 	
 	<div class="sec_three_box">
 		
+		<a href="">
 		
+		<div class="overlay">
+			
+			<span class="sec_box_title">Pricing</span><!-- sec_box_title -->
+			
+		</div><!-- overlay -->
+		
+		<img src="<?php bloginfo('template_directory');?>/images/pricing.jpg"/>
+		
+		</a>
 		
 	</div><!-- sec_three_box -->
 	
 	<div class="sec_three_box">
 		
+		<a href="">
 		
+		<div class="overlay">
+			
+			<span class="sec_box_title">Faqs</span><!-- sec_box_title -->
+			
+		</div><!-- overlay -->
+		
+		<img src="<?php bloginfo('template_directory');?>/images/faqs.jpg"/>
+		
+		</a>
 		
 	</div><!-- sec_three_box -->
 	
