63df007730cc698daba22a5b01212da47fa275ec
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-17 20:01:54 -0800

    push

---
+++ wp-content/themes/blink/homepage_template_parts/section-4.php
@@ -0,0 +1,66 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner content">
+		
+		<h1 class="intro">San Diego’s Premier Lash Boutique</h1>
+			
+			<div class="sec_four_main">
+			
+				<div class="sec_four_content">
+			
+				<p> Blink Lash Boutique started in a tiny one room studio in Mission Valley with Janelle Koch who was passionate about being one of the first Eyelash Extension specialists in San Diego. Having been an esthetician for several years already she knew the industry well and was dedicated to providing the best the industry had to offer. It didn’t take long before she was outgrowing her space and brought on friend and fellow esthetician Jen Pearson to help.</p>
+
+				<p>Since then they have continued to grow and together they have trained several new and aspiring Lash Stylist while preserving their original dedication to quality and customer service.</p>
+
+				<blockquote>“We attribute our success to our hard work and passion, but mostly to our wonderful clients who have been loyal over the years and become some of our good friends along the way. We would not be where we are at today without you and we love you!”</blockquote>
+			
+				<p>In May of 2010 they opened their Solana Beach location and then in September of 2012 opened their 3rd location Downtown San Diego.</p>
+			
+			</div><!-- sec_four_content -->
+			
+			<div class="sec_four_image_wrapper">
+				
+				<span class="sec_four_caption">Left to Right:  Janelle KOCH, Jen Pearson</span>
+				
+				<img src="<?php bloginfo('template_directory');?>/images/meet.jpg"/>
+				
+				
+				<div class="meet_link_wrapper">
+				
+					<div class="meet_link">
+						
+						<a href="">
+						
+						<?php echo file_get_contents("wp-content/themes/blink/images/view-more.svg"); ?>
+						
+						<span class="meet_title">Meet Janelle Koch</span><!-- meet_title -->
+						
+						<span class="meet_caption">(Founder/Owner</span><!-- meet_caption -->
+						
+						</a>
+						
+					</div><!-- meet_link -->
+					
+					<div class="meet_link">
+						
+						<a href="">
+						
+						<?php echo file_get_contents("wp-content/themes/blink/images/view-more.svg"); ?>
+						
+						<span class="meet_title">Meet Jen Pearson</span><!-- meet_title -->
+						
+						<span class="meet_caption">(Owner/Senior Lash Stylist)</span><!-- meet_caption -->
+						
+						</a>
+						
+					</div><!-- meet_link -->
+				
+				</div><!-- meet_link_wrapper -->
+				
+			</div><!-- sec_four_image_wrapper -->
+		
+		</div><!-- sec_four_main -->
+		
+	</div><!-- sec_four_inner -->
+	
+</section><!-- section_four -->
---
+++ wp-content/themes/blink/homepage_template_parts/section-5.php
@@ -0,0 +1,56 @@
+<section id="section_five">
+	
+	
+	<div class="sec_five_left"></div><!-- sec_five_left -->
+	
+	<div class="sec_five_right">
+		
+		<div class="review_wrapper">
+			
+			<?php echo file_get_contents("wp-content/themes/blink/images/blink-circle.svg"); ?>
+			
+			<div class="review_slideshow">
+				
+				<div class="single_slide">
+					
+					<span class="review_keyword">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name">Client Name</span><!-- reviews_name -->
+					
+				</div><!-- single_slide -->
+				
+			</div><!-- review_slideshow -->
+			
+			<a class="leave_review" href="">Leave a Review</a><!-- leave_review -->
+			
+			<div class="prev_next_wrapper">
+				
+				<div class="prev"></div><!-- prev -->
+				
+				<div class="next"></div><!-- next -->
+				
+			</div><!-- prev_next_wrapper -->
+			
+		</div><!-- review_wrapper -->
+		
+	</div><!-- sec_five_right -->
+	
+	
+</section><!-- section_five -->
