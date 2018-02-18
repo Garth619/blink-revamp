2f3222c172e82efef9fc2b8c6d683b9739e8fad7
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-18 14:18:09 -0800

    push

---
+++ wp-content/themes/blink/homepage_template_parts/section-6.php
@@ -0,0 +1,16 @@
+<section id="section_six">
+	
+	<div class="sec_six_inner">
+		
+		<?php echo file_get_contents("wp-content/themes/blink/images/accent.svg"); ?>
+		
+		<span class="signup_title">Sign Up For Our Latest Updates!</span><!-- signup_title -->
+		
+		<div class="signup_description">
+			
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.			
+		</div><!-- signup_description -->
+		
+	</div><!-- sec_six_inner -->
+	
+</section><!-- section_six -->
