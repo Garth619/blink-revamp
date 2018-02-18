deb8f2f7e8bc988ae56d26ec0da791b8aad624df
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-18 15:57:55 -0800

    push

---
+++ wp-content/themes/blink/homepage_template_parts/section-7.php
@@ -0,0 +1,140 @@
+<section id="section_seven">
+	
+	<div class="blog_wrapper">
+		
+		
+		<div class="single_post">
+			
+			<a class="" href="">
+			
+			<div class="single_post_image_wrapper">
+			
+				<div class="single_post_image_inner"></div><!-- single_post_image_wrapper -->
+			
+			</div><!-- single_post_image_wrapper -->
+			
+			<div class="single_post_content">
+				
+				<div class="tri_wrapper">
+					
+					<div class="tri"></div><!-- tri -->
+					
+				</div><!-- tri_wrapper -->
+				
+				<span class="blog_title">Blog Title</span><!-- blog_title -->
+				
+				<span class="meta">December 12th, 2017</span>
+				
+				<span class="blog_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute...</span><!-- blog_description -->
+				
+				<span class="read_more">Read More</span><!-- read_more -->
+				
+				
+			</div><!-- single_post_content -->
+			
+			</a>
+			
+		</div><!-- single_post -->
+		
+		<div class="single_post">
+			
+			<a class="" href="">
+			
+			<div class="single_post_image_wrapper">
+			
+				<div class="single_post_image_inner"></div><!-- single_post_image_wrapper -->
+			
+			</div><!-- single_post_image_wrapper -->
+			
+			<div class="single_post_content">
+				
+				<div class="tri_wrapper">
+					
+					<div class="tri"></div><!-- tri -->
+					
+				</div><!-- tri_wrapper -->
+				
+				<span class="blog_title">Blog Title</span><!-- blog_title -->
+				
+				<span class="meta">December 12th, 2017</span>
+				
+				<span class="blog_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute...</span><!-- blog_description -->
+				
+				<span class="read_more">Read More</span><!-- read_more -->
+				
+				
+			</div><!-- single_post_content -->
+			
+			</a>
+			
+		</div><!-- single_post -->
+		
+		<div class="single_post">
+			
+			<a class="" href="">
+			
+			<div class="single_post_image_wrapper">
+			
+				<div class="single_post_image_inner"></div><!-- single_post_image_wrapper -->
+			
+			</div><!-- single_post_image_wrapper -->
+			
+			<div class="single_post_content">
+				
+				<div class="tri_wrapper">
+					
+					<div class="tri"></div><!-- tri -->
+					
+				</div><!-- tri_wrapper -->
+				
+				<span class="blog_title">Blog Title</span><!-- blog_title -->
+				
+				<span class="meta">December 12th, 2017</span>
+				
+				<span class="blog_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute...</span><!-- blog_description -->
+				
+				<span class="read_more">Read More</span><!-- read_more -->
+				
+				
+			</div><!-- single_post_content -->
+			
+			</a>
+			
+		</div><!-- single_post -->
+		
+		<div class="single_post">
+			
+			<a class="" href="">
+			
+			<div class="single_post_image_wrapper">
+			
+				<div class="single_post_image_inner"></div><!-- single_post_image_wrapper -->
+			
+			</div><!-- single_post_image_wrapper -->
+			
+			<div class="single_post_content">
+				
+				<div class="tri_wrapper">
+					
+					<div class="tri"></div><!-- tri -->
+					
+				</div><!-- tri_wrapper -->
+				
+				<span class="blog_title">Blog Title</span><!-- blog_title -->
+				
+				<span class="meta">December 12th, 2017</span>
+				
+				<span class="blog_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute...</span><!-- blog_description -->
+				
+				<span class="read_more">Read More</span><!-- read_more -->
+				
+				
+			</div><!-- single_post_content -->
+			
+			</a>
+			
+		</div><!-- single_post -->
+		
+	</div><!-- blog_wrapper -->
+	
+</section><!-- section_seven -->
