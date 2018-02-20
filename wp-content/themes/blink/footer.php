a38ec3b78b98168964a6fc865f48d10b31259188
Garrett Cullen <Garth619@users.noreply.github.com>
2018-02-19 21:11:43 -0800

    push

--- wp-content/themes/blink/footer.php
+++ wp-content/themes/blink/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	
-	<div class="locations_wrapper">
+	<section class="locations_wrapper">
 		
 		<span class="large_header">Book an Appointment Today!</span><!-- large_header -->
 		
@@ -51,7 +51,68 @@
 			
 		</div><!-- location_col -->
 		
-	</div><!-- locations_wrapper -->
+		<div class="location_col">
+			
+			<span class="location_title">Mission Valley</span><!-- location_title -->
+			
+			<a class="address" href="">7801 Mission Center Ct., Suite 104<br/>San Diego, CA 92108</a>
+			
+			<ul class="hours">
+				<li>Tuesday – Friday 9AM - 7PM</li>
+				<li>Saturday 9AM - 3PM</li>
+				<li>Closed Sunday</li>
+				<li>Closed Monday</li>
+			</ul>
+			
+			<a class="footer_tel" href="tel:619 964-6082">
+				
+				<?php echo file_get_contents("wp-content/themes/blink/images/tel.svg"); ?>
+				
+				<span>(619) 964-6082</span>
+			
+			</a>
+			
+			<span class="leave_comment" href="">
+			
+				<?php echo file_get_contents("wp-content/themes/blink/images/email.svg"); ?>
+			
+				<span>Leave a Comment</span>
+			
+			</span><!-- leave_comment -->
+			
+			<div class="social_icons">
+				
+				
+				<a class="" href=""><?php echo file_get_contents("wp-content/themes/blink/images/fb.svg"); ?></a>
+				
+				<a class="" href=""><?php echo file_get_contents("wp-content/themes/blink/images/instagram.svg"); ?></a>
+				
+				<a class="" href=""><?php echo file_get_contents("wp-content/themes/blink/images/google-plus.svg"); ?></a>
+				
+				<a class="" href=""><?php echo file_get_contents("wp-content/themes/blink/images/twitter.svg"); ?></a>
+				
+				
+			</div><!-- social_icons -->
+			
+			<a class="location_book_now" href="">Book Now!</a><!-- location_book_now -->
+			
+		</div><!-- location_col -->
+		
+	</section><!-- locations_wrapper -->
+	
+	<section id="form_section">
+		
+		
+		<div class="form_wrapper">
+			
+			<span class="form_title">Got a Question? Get in Touch</span><!-- form_title -->
+			
+			<?php gravity_form(2, false, false, false, '', true, 546); ?>
+			
+		</div><!-- form_wrapper -->
+		
+		
+	</section><!-- form_section -->
 	
 </footer>
 
