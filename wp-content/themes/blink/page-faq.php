40696cb1aa6d5728fc31059ab5d3bf93c7ed83a8
Garrett Cullen <Garth619@users.noreply.github.com>
2018-03-03 19:21:07 -0800

    push

---
+++ wp-content/themes/blink/page-faq.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Template Name: FAQs */
+
+get_header(); ?>
+
+
+<div class="internal_main">
+	
+	
+	<div class="container content">
+		
+		<h1 class="internal"><?php the_title();?></h1>
+		
+				
+	</div><!-- container -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+
+
+	
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/blink/page-partnersavings.php
+++ wp-content/themes/blink/page-partnersavings.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	
 	<div class="container content">
 		
-		<h1 class="internal"><?php the_title();?></h1>
+		<h1 class="internal center"><?php the_title();?></h1>
 		
 		<div class="partner_wrapper">
 			
@@ -19,6 +19,30 @@ get_header(); ?>
 				<h2>Angelica B Beauty</h2>
 				
 				<img class="partner_img" src="<?php bloginfo('template_directory');?>/images/angelica.jpg"/>
+				
+				<p>Angelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin ngelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin.</p>
+				
+				<span class="additional_description">$25 Brazilian wax + 20% all facials</span><!-- additional_description -->
+				
+			</div><!-- single_partner -->
+			
+			<div class="single_partner">
+				
+				<h2>Angelica B Beauty</h2>
+				
+				<img class="partner_img" src="<?php bloginfo('template_directory');?>/images/envie-logo.png"/>
+				
+				<p>Angelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin.</p>
+				
+				<span class="additional_description">$25 Brazilian wax + 20% all facials</span><!-- additional_description -->
+				
+			</div><!-- single_partner -->
+			
+			<div class="single_partner">
+				
+				<h2>Angelica B Beauty</h2>
+				
+				<img class="partner_img" src="<?php bloginfo('template_directory');?>/images/studiobarrelogo.jpg"/>
 				
 				<p>Angelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin.</p>
 				
