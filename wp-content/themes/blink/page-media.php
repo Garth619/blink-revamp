fde0e73b0b5ca00dc6d9997f3a4453eb380b63da
Garrett Cullen <Garth619@users.noreply.github.com>
2018-03-04 10:18:30 -0800

    opush

---
+++ wp-content/themes/blink/page-media.php
@@ -0,0 +1,134 @@
+<?php
+
+/* Template Name: Media */
+
+get_header(); ?>
+
+
+<div class="internal_main">
+	
+	
+	<div class="container content">
+		
+		<h1 class="internal center"><?php the_title();?></h1>
+		
+		
+		<div class="media_wrapper">
+			
+			<div class="single_month_wrapper">
+				
+				<h2>December 2015</h2>
+			
+				<div class="single_media_wrapper">
+					
+					<a href="">
+				
+					<div class="img_wrapper">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/placeholder.png"/>
+					
+					</div><!-- img_wrapper -->
+				
+					<div class="media_content">
+					
+						<h3>Perfect Your NYE Look With Tips From The Pros</h3>
+						
+						<span class="media_title">By Locale</span><!-- media_title -->
+						
+					</div><!-- media_content -->
+					
+					</a>
+				
+				</div><!-- single_media_wrapper -->
+				
+				<div class="single_media_wrapper">
+					
+					<a href="">
+				
+					<div class="img_wrapper">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/placeholder.png"/>
+					
+					</div><!-- img_wrapper -->
+				
+					<div class="media_content">
+					
+						<h3>Perfect Your NYE Look With Tips From The Pros</h3>
+						
+						<span class="media_title">By Locale</span><!-- media_title -->
+						
+					</div><!-- media_content -->
+					
+					</a>
+				
+				</div><!-- single_media_wrapper -->
+			
+			</div><!-- single_month_wrapper -->
+			
+			<div class="single_month_wrapper">
+				
+				<h2>December 2015</h2>
+			
+				<div class="single_media_wrapper">
+					
+					<a href="">
+				
+					<div class="img_wrapper">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/placeholder.png"/>
+					
+					</div><!-- img_wrapper -->
+				
+					<div class="media_content">
+					
+						<h3>Perfect Your NYE Look With Tips From The Pros</h3>
+						
+						<span class="media_title">By Locale</span><!-- media_title -->
+						
+					</div><!-- media_content -->
+					
+					</a>
+				
+				</div><!-- single_media_wrapper -->
+				
+				<div class="single_media_wrapper">
+					
+					<a href="">
+				
+					<div class="img_wrapper">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/placeholder.png"/>
+					
+					</div><!-- img_wrapper -->
+				
+					<div class="media_content">
+					
+						<h3>Perfect Your NYE Look With Tips From The Pros</h3>
+						
+						<span class="media_title">By Locale</span><!-- media_title -->
+						
+					</div><!-- media_content -->
+					
+					</a>
+				
+				</div><!-- single_media_wrapper -->
+			
+			</div><!-- single_month_wrapper -->
+			
+		</div><!-- media_wrapper -->
+		
+				
+	</div><!-- container -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+
+
+	
+
+
+
+<?php get_footer(); ?>
