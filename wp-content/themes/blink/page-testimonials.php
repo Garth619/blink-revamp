394019650fcfe53327e4ad7d30690908eaebabc0
Garrett Cullen <Garth619@users.noreply.github.com>
2018-03-03 20:06:29 -0800

    push

---
+++ wp-content/themes/blink/page-testimonials.php
@@ -0,0 +1,197 @@
+<?php
+
+/* Template Name: Testimonials */
+
+get_header(); ?>
+
+
+<div class="internal_main">
+	
+	
+	<div class="container content">
+		
+		<h1 class="internal center"><?php the_title();?></h1>
+		
+		<div class="testominals_wrapper_page">
+			
+			
+			<div class="single_testimonial_page">
+				
+				
+				<span class="review_keyword_page">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars_page">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content_page">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name_page">Client Name</span><!-- reviews_name -->
+				
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial_page">
+				
+				
+				<span class="review_keyword_page">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars_page">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content_page">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name_page">Client Name</span><!-- reviews_name -->
+				
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial_page">
+				
+				
+				<span class="review_keyword_page">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars_page">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content_page">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name_page">Client Name</span><!-- reviews_name -->
+				
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial_page">
+				
+				
+				<span class="review_keyword_page">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars_page">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content_page">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name_page">Client Name</span><!-- reviews_name -->
+				
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial_page">
+				
+				
+				<span class="review_keyword_page">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars_page">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content_page">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name_page">Client Name</span><!-- reviews_name -->
+				
+				
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial_page">
+				
+				
+				<span class="review_keyword_page">"Exceptional"</span><!-- review_keyword -->
+					
+					<div class="stars_page">
+						
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+						<span>&starf;</span>
+					
+					</div><!-- stars -->
+					
+					<div class="reviews_content_page">
+						
+					“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste”
+						
+						
+					</div><!-- reviews_content -->
+					
+					<span class="reviews_name_page">Client Name</span><!-- reviews_name -->
+				
+				
+			</div><!-- single_testimonial -->
+			
+			
+		</div><!-- testominals_wrapper -->
+				
+				
+	</div><!-- container -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+
+
+	
+
+
+
+<?php get_footer(); ?>
