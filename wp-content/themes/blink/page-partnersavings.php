5a32f04a9650d7dd912ea6166894451e3b918125
Garrett Cullen <Garth619@users.noreply.github.com>
2018-03-03 19:05:54 -0800

    push

---
+++ wp-content/themes/blink/page-partnersavings.php
@@ -0,0 +1,68 @@
+<?php
+
+/* Template Name: Partner Savings */
+
+get_header(); ?>
+
+
+<div class="internal_main">
+	
+	
+	<div class="container content">
+		
+		<h1 class="internal"><?php the_title();?></h1>
+		
+		<div class="partner_wrapper">
+			
+			<div class="single_partner">
+				
+				<h2>Angelica B Beauty</h2>
+				
+				<img class="partner_img" src="<?php bloginfo('template_directory');?>/images/angelica.jpg"/>
+				
+				<p>Angelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin.</p>
+				
+				<span class="additional_description">$25 Brazilian wax + 20% all facials</span><!-- additional_description -->
+				
+			</div><!-- single_partner -->
+			
+			<div class="single_partner">
+				
+				<h2>Angelica B Beauty</h2>
+				
+				<img class="partner_img" src="<?php bloginfo('template_directory');?>/images/angelica.jpg"/>
+				
+				<p>Angelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin.</p>
+				
+				<span class="additional_description">$25 Brazilian wax + 20% all facials</span><!-- additional_description -->
+				
+			</div><!-- single_partner -->
+			
+			<div class="single_partner">
+				
+				<h2>Angelica B Beauty</h2>
+				
+				<img class="partner_img" src="<?php bloginfo('template_directory');?>/images/angelica.jpg"/>
+				
+				<p>Angelica B Beauty is a charming studio specializing in body waxing and facial care. With expert estheticians, Angelica B Beauty is dedicated to helping women achieve healthy, beautiful skin.</p>
+				
+				<span class="additional_description">$25 Brazilian wax + 20% all facials</span><!-- additional_description -->
+				
+			</div><!-- single_partner -->
+			
+		</div><!-- partner_wrapper -->
+		
+	</div><!-- container -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+
+
+	
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/blink/page.php
+++ wp-content/themes/blink/page.php
@@ -6,15 +6,9 @@
 	
 	<div class="container content">
 		
-		<h1 class="internal">Header One</h1>
+		<h1 class="internal"><?php the_title();?></h1>
 		
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do <a href="">eiusmod</a> tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore</p>
-		
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore</p>
-		
-		<h2>Header Two</h2>
-		
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore</p>
+		<?php get_template_part( 'loop', 'page' ) ?>
 		
 	</div><!-- container -->
 	
@@ -23,7 +17,7 @@
 
 
 
-<?php //use get_template_part( 'loop', 'page' ) if needed ?>
+
 
 	
 
