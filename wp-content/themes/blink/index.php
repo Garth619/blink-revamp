a4a54d1a14b9167ebeba8fbe42a92c40f1f4afa2
Garrett Cullen <Garth619@users.noreply.github.com>
2018-03-04 15:25:26 -0800

    push

--- wp-content/themes/blink/index.php
+++ wp-content/themes/blink/index.php
@@ -1,9 +1,177 @@
 <?php get_header(); ?>
 
 
-<?php get_template_part( 'loop', 'index' );?>
+
+<div class="internal_main">
+	
+	
+	<div class="container content blog_container">
+		
+		<?php get_sidebar('blog'); ?>
+		
+		<div class="blog_content">
+			
+			<h1 class="internal">Blog</h1>
+			
+			<?php // get_template_part( 'loop', 'index' );?>
+			
+			<div class="blog_feed">
+				
+				
+				<div class="single_post">
+					
+					<a class="" href="">
+					
+						<div class="blog_img_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/eyelash-extensions.jpg"/>
+						
+						</div><!-- blog_img_wrapper -->
+						
+						<div class="single_content_wrapper">
+						
+							<h2>Meet Janelle!</h2>
+						
+							<span class="meta"><?php $pfx_date = get_the_date(); echo $pfx_date ?></span><!-- meta -->
+					
+							<span class="single_post_excerpt">Read all about Janelle Koch of Blink Lash Boutique in this informative SDVoyager article!  How was Blink Lash Boutique founded and what makes Blink special? Meet Janelle Koch of Blink Lash Boutique in Mission Valley and Solana Beach </span><!-- blog_excerpt -->
+						
+						</div><!-- single_content_wrapper -->
+					
+					</a>
+					
+				</div><!-- single_post -->
+				
+				<div class="single_post">
+					
+					<a class="" href="">
+					
+						<div class="blog_img_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/meet.jpg"/>
+						
+						</div><!-- blog_img_wrapper -->
+						
+						<div class="single_content_wrapper">
+						
+							<h2>Meet Janelle!</h2>
+						
+							<span class="meta"><?php $pfx_date = get_the_date(); echo $pfx_date ?></span><!-- meta -->
+					
+							<span class="single_post_excerpt">Read all about Janelle Koch of Blink Lash Boutique in this informative SDVoyager article!  How was Blink Lash Boutique founded and what makes Blink special? Meet Janelle Koch of Blink Lash Boutique in Mission Valley and Solana Beach </span><!-- blog_excerpt -->
+						
+						</div><!-- single_content_wrapper -->
+					
+					</a>
+					
+				</div><!-- single_post -->
+				
+				<div class="single_post">
+					
+					<a class="" href="">
+					
+						<div class="blog_img_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/eyelash-extensions.jpg"/>
+						
+						</div><!-- blog_img_wrapper -->
+						
+						<div class="single_content_wrapper">
+						
+							<h2>Meet Janelle!</h2>
+						
+							<span class="meta"><?php $pfx_date = get_the_date(); echo $pfx_date ?></span><!-- meta -->
+					
+							<span class="single_post_excerpt">Read all about Janelle Koch of Blink Lash Boutique in this informative SDVoyager article!  How was Blink Lash Boutique founded and what makes Blink special? Meet Janelle Koch of Blink Lash Boutique in Mission Valley and Solana Beach </span><!-- blog_excerpt -->
+						
+						</div><!-- single_content_wrapper -->
+					
+					</a>
+					
+				</div><!-- single_post -->
+				
+				<div class="single_post">
+					
+					<a class="" href="">
+					
+						<div class="blog_img_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/eyelash-extensions.jpg"/>
+						
+						</div><!-- blog_img_wrapper -->
+						
+						<div class="single_content_wrapper">
+						
+							<h2>Meet Janelle!</h2>
+						
+							<span class="meta"><?php $pfx_date = get_the_date(); echo $pfx_date ?></span><!-- meta -->
+					
+							<span class="single_post_excerpt">Read all about Janelle Koch of Blink Lash Boutique in this informative SDVoyager article!  How was Blink Lash Boutique founded and what makes Blink special? Meet Janelle Koch of Blink Lash Boutique in Mission Valley and Solana Beach </span><!-- blog_excerpt -->
+						
+						</div><!-- single_content_wrapper -->
+					
+					</a>
+					
+				</div><!-- single_post -->
+				
+				<div class="single_post">
+					
+					<a class="" href="">
+					
+						<div class="blog_img_wrapper">
+						
+							<img src="<?php bloginfo('template_directory');?>/images/eyelash-extensions.jpg"/>
+						
+						</div><!-- blog_img_wrapper -->
+						
+						<div class="single_content_wrapper">
+						
+							<h2>Meet Janelle!</h2>
+						
+							<span class="meta"><?php $pfx_date = get_the_date(); echo $pfx_date ?></span><!-- meta -->
+					
+							<span class="single_post_excerpt">Read all about Janelle Koch of Blink Lash Boutique in this informative SDVoyager article!  How was Blink Lash Boutique founded and what makes Blink special? Meet Janelle Koch of Blink Lash Boutique in Mission Valley and Solana Beach </span><!-- blog_excerpt -->
+						
+						</div><!-- single_content_wrapper -->
+					
+					</a>
+					
+				</div><!-- single_post -->
+				
+				
+				
+				
+				
+				
+			</div><!-- blog_feed -->
+			
+		</div><!-- blog_content -->
+				
+	</div><!-- container -->
+	
+	
+</div><!-- internal_main -->
 
 
-<?php // get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
