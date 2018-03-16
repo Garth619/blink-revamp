ca012b818b3db234e7870c20163d40e1099e10f5
Garrett Cullen <Garth619@users.noreply.github.com>
2018-03-15 22:03:25 -0700

    push

---
+++ wp-content/themes/blink/page-allservices.php
@@ -0,0 +1,159 @@
+<?php 
+	
+
+/* Template Name: All Services */
+
+get_header(); ?>
+
+
+<div class="internal_main">
+	
+	
+	<div class="container content">
+		
+		<h1 class="internal center"><?php the_title();?></h1>
+		
+		<?php // get_template_part( 'loop', 'page' ) ?>
+		
+		
+		<div class="all_services_wrapper">
+			
+			
+			<?php echo file_get_contents("wp-content/themes/blink/images/blink-circle.svg"); ?>
+			
+			
+			<blockquote><span>Be everything you've aspired to be in the world  - starting with your own.</span> <span>Say something or nothing at all. Words are useless. Eyes are fluent. Don't just be. Become.</span></blockquote>
+			
+			
+			<p>Fashion critics, beauty enthusiasts and industry professionals nationwide have acknowledged Blink Lash Boutique as the premier eyelash extension studio of Southern California. Several years of superb service, sophisticated presence and unparalleled results have made Blink Lash Boutique the only choice for 5 star treatment and quality lashes.</p>
+
+<p>Your look says everything. Who you are. Who you’re not. Your aspirations and passions. Your truth. Your beauty. Yourself. Blink Lash Boutique stands for everything you are – a life of beauty without boundaries.</p>
+
+	<h2>Classic Lashes vs Volume Lashes</h2>
+	
+	<img class="alignleft" width="450" src="<?php bloginfo('template_directory');?>/images/ba-blush-volume-classic.jpg"/>
+	
+	<p>Choosing the right look for you is important, which is why at Blink Lash Boutique we offer a variety of looks with different degrees of fullness, amount of extensions applied, and style of extensions.  Many people coming in for lash extensions are looking for a lot of fullness or volume, but not everyone has enough natural lashes to achieve the look they want with classic lash extensions.  Sometimes even women who are blessed with thick and full natural lashes want an even fuller look than what classic lashes can give them, and they opt for volume extensions to maximize their fullness.</p>
+
+<p>The difference between classic extensions and volume extension is the number of extensions placed on each natural lash.  Classic lash extensions is the traditional technique of applying eyelash extensions.  It is a ratio of 1 extension to 1 natural lash.  Volume lash extensions, also called Russian volume, is creating a fan of several finer extensions and applying it to one natural lash.  It’s most commonly a ratio of 3-5 lashes to one natural lash. (See photo below)</p>
+
+<p>Typically with classic extensions the extension used is between .10mm to .15mm in diameter.  The extensions used to create the fans for volume application are .05mm or .07mm.  So each individual lash in the volume fan is finer than the individual classic extension.  Classic extensions tend to create more of a mascara look, with each strand thicker. Volume extensions create a fuller and fluffier look and help fill in any gaps in natural lashes.  If fullness is what you’re after and you have finer or sparse natural lashes, volume extensions will help you achieve more fullness than you could with classic extensions.</p>
+
+<p>Since both styles of extensions are attached to each natural lash and not the skin, we are only able to apply as many classic or volume extensions as the number of natural lashes you have.  So the more natural lashes an individual has, the fuller and thicker we can get their extensions.  The finer and more sparse the natural lashes are, the more limited we are in the amount of extensions we can apply.</p>
+
+<p>Since the volume technique requires a lot more skill and training, and the application takes longer, the cost for volume extensions is higher.  Please see pricing below for comparison.</p>
+
+<img width="300" src="<?php bloginfo('template_directory');?>/images/l.jpg"/>
+
+<h2>Artistry</h2>
+
+<p>All Blink trained lash stylists are meticulous in elevating your eyes to a brand new you. We create your perfect look – guaranteed.</p>
+
+<h2>Pricing</h2>
+
+<p>No matter your desired look, we’re certain to achieve your every expectation while your purses stay nice and happy!</p>
+
+
+<h2>Services</h2>
+
+<p>We embody both professionalism and class in treating our clients – a passion that grows alongside our expanding Blink family.</p>
+
+
+
+
+<div class="allservices_pricing">
+	
+	
+	<div class="single_allserv_price">
+		
+		<span class="allserve_price_title">Bashful</span><!-- allserve_price_title -->
+		
+		<p>Kill ’em softly with the subtle yet elegant look of BASHFUL. This modest set let’s you say everything you need, effortlessly.</p>
+		
+		<a class="see_pricing_button" href="">See Pricing</a>
+		
+	</div><!-- single_allserv_price -->
+	
+	<div class="single_allserv_price">
+		
+		<span class="allserve_price_title">Blush</span><!-- allserve_price_title -->
+		
+		<p>Our most popular set for new clients!  Go ahead, throw away your mascara and lash curler.  The dazzling look of BLUSH is sure to have you turning heads.</p>
+		
+		<a class="see_pricing_button" href="">See Pricing</a>
+		
+	</div><!-- single_allserv_price -->
+	
+	<div class="single_allserv_price">
+		
+		<span class="allserve_price_title">Bombshell</span><!-- allserve_price_title -->
+		
+		<p>Want to make jaws drop?  Be a BOMBSHELL beauty ready for your big night out, or even while you’re running errands.  Just be ready for all of the extra attention you’re about to get!</p>
+		
+		<a class="see_pricing_button" href="">See Pricing</a>
+		
+	</div><!-- single_allserv_price -->
+	
+	<div class="single_allserv_price">
+		
+		<span class="allserve_price_title">Touch Ups</span><!-- allserve_price_title -->
+		
+		<a class="see_pricing_button" href="">See Pricing</a>
+		
+	</div><!-- single_allserv_price -->
+	
+	<div class="single_allserv_price">
+		
+		<span class="allserve_price_title">Lash Lifts</span><!-- allserve_price_title -->
+		
+		<a class="see_pricing_button" href="">See Pricing</a>
+		
+	</div><!-- single_allserv_price -->
+	
+	<div class="single_allserv_price">
+		
+		<span class="allserve_price_title">Bottom Lashes</span><!-- allserve_price_title -->
+		
+		<p>15 lashes per eye TOUCH UP 10 lashes per eye.</p>
+		
+		<a class="see_pricing_button" href="">See Pricing</a>
+		
+	</div><!-- single_allserv_price -->
+	
+	
+</div><!-- allservices_pricing -->
+		
+		<div class="additional_services">
+			
+			<h3>Additional Services</h3>
+			
+			<ul>
+				
+				<li>Facial Waxing</li>
+				<li>Brow Tinting</li>
+				<li>Lash Tinting</li>
+				<li>Strip Lashes</li>
+				
+			</ul>
+			
+			<a class="guarantee" href="<?php the_permalink(37);?>">Learn about our satisfaction guarantee!</a>
+			
+		</div><!-- additional_services -->
+			
+		</div><!-- all_services_wrapper -->
+		
+		
+	</div><!-- container -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+
+
+	
+
+
+
+<?php get_footer(); ?>
